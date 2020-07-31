56e64b91cc5abdd9af02b2194a552ade1885b126
sathwikreddygv <49616121+sathwikreddygv@users.noreply.github.com>
2020-07-31 12:34:09 +0530

    Added comments

--- client.py
+++ client.py
@@ -34,24 +34,39 @@ while True:
 		client_socket.send(message_header + message)
 
 	try:
-		while True:
-			username_header = client_socket.recv(MAX_LENGTH)
+        #incase of receiving msgs from the server
+        while True: 
+            #receive the username header
+            username_header = client_socket.recv(MAX_LENGTH)
 
-			if not len(username_header):
-				print('connection closed by the server')
-				sys.exit()
-			username_len = int(username_header.decode('utf-8').strip())
-			username = client_socket.recv(username_len).decode('utf-8')
-			message_header = client_socket.recv(MAX_LENGTH)
-			message_len = int(message_header.decode('utf-8').strip())
-			message = client_socket.recv(message_len).decode('utf-8')
-			print(f"{username} > {message}")
-	except IOerror as e:
-		if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
-			print("Reading error", str(e))
-			sys.exit()
-		continue
+            #if we don't receive any data, connection is lost
+            if not len(username_header):
+                print('connection closed by the server')
+                sys.exit()
 
-	except Exception as e:
-		print("general error", str(e))
-		sys.exit()
+            #get the length of username
+            username_len = int(username_header.decode('utf-8').strip())
+            #receive the actual username now
+            username = client_socket.recv(username_len).decode('utf-8')
+
+            #do the similar thing for the message
+            message_header = client_socket.recv(MAX_LENGTH)
+            message_len = int(message_header.decode('utf-8').strip())
+            message = client_socket.recv(message_len).decode('utf-8')
+
+            #then print the message
+            print(f"{username} > {message}")
+
+    except IOError as e:
+        # This is normal on non blocking connections - when there are no incoming data error is going to be raised
+        # Some operating systems will indicate that using AGAIN, and some using WOULDBLOCK error code
+        # We are going to check for both - if one of them - that's expected, means no incoming data, continue as normal
+        # If we got different error code - something happened
+        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+            print("Reading error", str(e))
+            sys.exit()
+        continue #we didn't receive anything
+
+    except Exception as e:# Any other exception - something happened, exit
+        print("general error", str(e))
+        sys.exit()
